87d12d93e02c22aa4a3778ecf33d40b9f3c44118
Mark Cafaro <34887852+mcafaro@users.noreply.github.com>
2024-04-15 15:11:35 -0400

    Update and rename myscript.m to myfunc.m

---
+++ myfunc.m
@@ -0,0 +1,7 @@
+function out = myfunc
+  disp('hello world');
+  disp("this script is displaying");
+  disp 'in a few different ways';
+  disp here
+  out = [1 2 3];
+end
--- myscript.m
+++
@@ -1,4 +0,0 @@
-disp('hello world');
-disp("this script is displaying");
-disp 'in a few different ways';
-disp here
